7bb515a0c3377f31e13e2949a4e0e15c3b699806
Salma Elsoly <salmaelsoly@gmail.com>
2023-12-25 23:55:42 +0200

    account controller implementation

--- src/api/account/index.js
+++ src/api/account/index.js
@@ -1,7 +1,13 @@
 const { Router } = require('express');
-const { changeName } = require('../../controllers/account/AccountController');
+const { changeName, changeEmail, changePassword, changeAddress, changePhone, addBankAccount } = require('../../controllers/account/AccountController');
 const router = Router();
 
 router.post('/name', changeName);
+router.post('/email', changeEmail);
+router.post('/password', changePassword);
+router.post('/address', changeAddress);
+router.post('/phone', changePhone);
+router.post('/add_bank_account', addBankAccount);
+
 
 module.exports = router;
--- src/controllers/account/AccountController.js
+++ src/controllers/account/AccountController.js
@@ -1,8 +1,101 @@
-// const { PrismaClient } = require("@prisma/client");
-// const client = new PrismaClient();
+const { PrismaClient } = require('@prisma/client');
+const client = new PrismaClient();
 
-const changeName = () => {};
+const changeName = (req, res) => {
+  client.user
+    .update({
+      where: {
+        SSN: req.user.SSN,
+      },
+      data: {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+      },
+    })
+    .then((user) => {
+      res.json(user);
+    });
+};
+const changeEmail = (req, res) => {
+  client.user
+    .update({
+      where: {
+        SSN: req.user.SSN,
+      },
+      data: {
+        email: req.body.email,
+      },
+    })
+    .then((user) => {
+      res.json(user);
+    });
+};
+const changePassword = (req, res) => {
+  client.user
+    .update({
+      where: {
+        SSN: req.user.SSN,
+      },
+      data: {
+        password: req.body.password,
+      },
+    })
+    .then((user) => {
+      res.json(user);
+    });
+};
+
+const changeAddress = (req, res) => {
+  client.user
+    .update({
+      where: {
+        SSN: req.user.SSN,
+      },
+      data: {
+        address: req.body.address,
+      },
+    })
+    .then((user) => {
+      res.json(user);
+    });
+};
+
+const changePhone = (req, res) => {
+  client.user
+    .update({
+      where: {
+        SSN: req.user.SSN,
+      },
+      data: {
+        phone: req.body.phone,
+      },
+    })
+    .then((user) => {
+      res.json(user);
+    });
+};
+
+const addBankAccount = (req, res) => {
+  const currentDateTime = new Date();
+  client.bankAccount.create({
+    data: {
+      accountNumber: req.body.accountNumber,
+      openingDate: currentDateTime,
+      balance: req.body.balance,
+      SSN: req.user.SSN,
+      swiftcode: req.body.swiftcode,
+    },
+  })
+    .then((account) => {
+      res.json(account);
+    });
+};
 
 module.exports = {
   changeName,
+  changeEmail,
+  changePassword,
+  changeAddress,
+  changePhone,
+  addBankAccount,
 };
